c66653ddbdb419981ab8c477eb4d5c83aeea0f25
anvian <andre.carbajal.vargas@gmail.com>
2022-08-13 22:00:54 -0500

    Basic Mod

---
+++ src/main/java/net/anvian/bee_info/mixin/ExampleMixin.java
@@ -0,0 +1,16 @@
+package net.anvian.bee_info.mixin;
+
+import net.anvian.bee_info.MoreBeeInfoMod;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(TitleScreen.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "init()V")
+	private void init(CallbackInfo info) {
+		MoreBeeInfoMod.LOGGER.info("This line is printed by an example mod mixin!");
+	}
+}
