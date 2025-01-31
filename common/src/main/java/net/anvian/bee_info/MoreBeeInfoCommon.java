805f9e14ec358796affd3a11077c5b44f5c8319e
anviaan <andre.carbajal.vargas@gmail.com>
2025-01-31 17:08:09 -0500

    Add telemetry support and config path management for MoreBeeInfo

--- common/src/main/java/net/anvian/bee_info/MoreBeeInfoCommon.java
+++ common/src/main/java/net/anvian/bee_info/MoreBeeInfoCommon.java
@@ -1,11 +1,26 @@
 package net.anvian.bee_info;
 
+import net.anvian.anvianslib.config.TelemetryConfigManager;
+import net.anvian.anvianslib.util.LibUtil;
 import net.anvian.bee_info.platform.Services;
 
 public class MoreBeeInfoCommon {
     public static void init() {
         if (Services.PLATFORM.isModLoaded(Constants.MOD_ID)) {
             Constants.LOG.info("Hello from " + Constants.MOD_ID);
+
+            LibUtil.generateConfigPath(Constants.MOD_ID, Services.PLATFORM.getGameConfigDirectory());
+
+            TelemetryConfigManager.initialize(Services.PLATFORM.getGameConfigDirectory().resolve(Constants.MOD_ID).toFile());
+            if (TelemetryConfigManager.getConfig().enableTelemetry){
+                TelemetryConfigManager.sendTelemetryData(
+                        Constants.MOD_ID,
+                        "1.4",
+                        LibUtil.getMinecraftVersion(),
+                        Services.PLATFORM.getPlatformName(),
+                        !Services.PLATFORM.isDevelopmentEnvironment()
+                );
+            }
         }
     }
 }
--- forge/src/main/java/net/anvian/bee_info/platform/ForgePlatformHelper.java
+++ forge/src/main/java/net/anvian/bee_info/platform/ForgePlatformHelper.java
@@ -3,6 +3,9 @@ package net.anvian.bee_info.platform;
 import net.anvian.bee_info.platform.services.IPlatformHelper;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.loading.FMLLoader;
+import net.minecraftforge.fml.loading.FMLPaths;
+
+import java.nio.file.Path;
 
 public class ForgePlatformHelper implements IPlatformHelper {
     @Override
@@ -18,5 +21,10 @@ public class ForgePlatformHelper implements IPlatformHelper {
     @Override
     public boolean isDevelopmentEnvironment() {
         return !FMLLoader.isProduction();
+    }
+
+    @Override
+    public Path getGameConfigDirectory() {
+        return FMLPaths.CONFIGDIR.get();
     }
 }
