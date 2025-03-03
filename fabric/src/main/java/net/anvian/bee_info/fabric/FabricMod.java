73bbd4e31a182005ce973836f847ac1a0370d4b9
anviaan <andre.carbajal.vargas@gmail.com>
2025-03-02 19:48:48 -0500

    port to 1.19

--- fabric/src/main/java/net/anvian/bee_info/fabric/FabricMod.java
+++ fabric/src/main/java/net/anvian/bee_info/fabric/FabricMod.java
@@ -1,11 +1,28 @@
 package net.anvian.bee_info.fabric;
 
+import net.anvian.anvianslib.config.TelemetryConfigManager;
+import net.anvian.anvianslib.util.LibUtil;
 import net.anvian.bee_info.CommonMod;
+import net.anvian.bee_info.Constants;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.loader.api.FabricLoader;
 
 public final class FabricMod implements ModInitializer {
     @Override
     public void onInitialize() {
         CommonMod.init();
+
+        LibUtil.generateConfigPath(Constants.MOD_ID, FabricLoader.getInstance().getConfigDir());
+
+        TelemetryConfigManager.initialize(FabricLoader.getInstance().getConfigDir().resolve(Constants.MOD_ID).toFile());
+        if (TelemetryConfigManager.getConfig().enableTelemetry) {
+            TelemetryConfigManager.sendTelemetryData(
+                    Constants.MOD_ID,
+                    "1.4",
+                    LibUtil.getMinecraftVersion(),
+                    "Fabric",
+                    !FabricLoader.getInstance().isDevelopmentEnvironment()
+            );
+        }
     }
 }
--- forge/src/main/java/net/anvian/bee_info/forge/ForgeMod.java
+++ forge/src/main/java/net/anvian/bee_info/forge/ForgeMod.java
@@ -1,12 +1,29 @@
 package net.anvian.bee_info.forge;
 
+import net.anvian.anvianslib.config.TelemetryConfigManager;
+import net.anvian.anvianslib.util.LibUtil;
 import net.anvian.bee_info.CommonMod;
 import net.anvian.bee_info.Constants;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.loading.FMLLoader;
+import net.minecraftforge.fml.loading.FMLPaths;
 
 @Mod(Constants.MOD_ID)
 public final class ForgeMod {
     public ForgeMod() {
         CommonMod.init();
+
+        LibUtil.generateConfigPath(Constants.MOD_ID, FMLPaths.CONFIGDIR.get());
+
+        TelemetryConfigManager.initialize(FMLPaths.CONFIGDIR.get().resolve(Constants.MOD_ID).toFile());
+        if (TelemetryConfigManager.getConfig().enableTelemetry) {
+            TelemetryConfigManager.sendTelemetryData(
+                    Constants.MOD_ID,
+                    "1.4",
+                    LibUtil.getMinecraftVersion(),
+                    "Forge",
+                    FMLLoader.isProduction()
+            );
+        }
     }
 }
