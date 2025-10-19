a063d1ae8fe45bf982587644fff06fe4bad62385
anviaan <andre.carbajal.vargas@gmail.com>
2025-10-19 14:54:36 -0500

    Update dependencies to use version variables and enhance mod compatibility

--- common/src/main/java/net/anvian/bee_info/CommonMod.java
+++ common/src/main/java/net/anvian/bee_info/CommonMod.java
@@ -1,7 +1,9 @@
 package net.anvian.bee_info;
 
+import net.anvian.anvianslib.util.LibUtil;
+
 public final class CommonMod {
     public static void init() {
-
+        LibUtil.setupTelemetry(Constants.MOD_ID, "1.4.1");
     }
 }
